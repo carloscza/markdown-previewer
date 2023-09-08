19c3fffa7c1dc6ab30b82fbee86f56b1662a6e0b
carloscza <carrloscarranza@gmail.com>
2023-09-08 12:44:34 -0700

    Added browser router

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App.js';
 import store from './store.js';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={store}>
-        <App />
-    </Provider>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </BrowserRouter>
 )
